d53a98cb467f50aed7c1aeddeee3751b82e0c73b
Arnaud Tanguy <arn.tanguy@gmail.com>
2021-11-24 18:16:02 +0100

    [cmake/bionic] Fix finding libqi

--- CMakeModules/Findmc_naoqi_3rd_party_qi.cmake
+++ CMakeModules/Findmc_naoqi_3rd_party_qi.cmake
@@ -10,35 +10,34 @@
 #
 
 if(NOT TARGET qi::qi)
-  # Look for libqi (as installed from lastest souce)
-  find_package(qi QUIET)
-  if(NOT ${QI_PACKAGE_FOUND})
-    # If not found, search using pkgconfig (e.g installed with ros-melodic-naoqi-libqi ros-melodic-naoqi-libqicore)
-    include(FindPkgConfig)
-    pkg_search_module(LIBQI naoqi_libqi naoqi_libqicore)
-    if(NOT LIBQI_FOUND)
-      message(FATAL_ERROR "Could not find the libqi package using CMake package or pkg-config")
-    endif()
+  # Search using pkgconfig (e.g installed with ros-melodic-naoqi-libqi ros-melodic-naoqi-libqicore)
+  include(FindPkgConfig)
+  pkg_search_module(LIBQI naoqi_libqi naoqi_libqicore)
+  if(LIBQI_FOUND)
     foreach(LIB ${LIBQI_LIBRARIES})
       find_library(${LIB}_FULL_PATH NAME ${LIB} HINTS ${LIBQI_LIBRARY_DIRS})
       list(APPEND LIBQI_FULL_LIBRARIES ${${LIB}_FULL_PATH})
     endforeach()
-    message("-- Found libqi libraries: ${LIBQI_FULL_LIBRARIES}")
+    message("-- Found libqi using pkgconfig - libraries: ${LIBQI_FULL_LIBRARIES}")
+    message("-- Found libqi using pkgconfig - include directories: ${LIBQI_INCLUDE_DIRS}")
     add_library(qi::qi INTERFACE IMPORTED)
     set_target_properties(qi::qi PROPERTIES
       INTERFACE_LINK_LIBRARIES "${LIBQI_FULL_LIBRARIES}"
+      INTERFACE_INCLUDE_DIRECTORIES "${LIBQI_INCLUDE_DIRS}"
     )
-    if(NOT "${LIBQI_INCLUDE_DIRS}" STREQUAL "")
-      set_target_properties(qi::qi PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${LIBQI_INCLUDE_DIRS}"
-      )
-      message("-- Found libqi include directories: ${LIBQI_INCLUDE_DIRS}")
-    endif()
   else()
-    add_library(qi::qi INTERFACE IMPORTED)
-    set_target_properties(qi::qi PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES ${QI_INCLUDE_DIRS}
-      INTERFACE_LINK_LIBRARIES ${QI_LIBRARIES}
-    )
+    # Look for libqi (as installed from lastest souce)
+    find_package(qi QUIET)
+    if(${QI_PACKAGE_FOUND})
+      add_library(qi::qi INTERFACE IMPORTED)
+      message("-- Found libqi using cmake - libraries: ${QI_LIBRARIES}")
+      message("-- Found libqi using cmake - include directories: ${QI_INCLUDE_DIRS}")
+      set_target_properties(qi::qi PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${QI_INCLUDE_DIRS}
+        INTERFACE_LINK_LIBRARIES ${QI_LIBRARIES}
+      )
+    else()
+      message(FATAL_ERROR "Could not find the libqi package using CMake package or pkg-config")
+    endif()
   endif()
 endif()
