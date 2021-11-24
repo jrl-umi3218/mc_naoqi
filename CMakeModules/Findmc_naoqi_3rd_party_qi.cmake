a909e6e516e80c5b7f7d25be07bdcb1151223776
Arnaud Tanguy <arn.tanguy@gmail.com>
2021-11-24 17:15:15 +0100

    [cmake] Also find libqi installed by ROS

--- CMakeModules/Findmc_naoqi_3rd_party_qi.cmake
+++ CMakeModules/Findmc_naoqi_3rd_party_qi.cmake
@@ -10,9 +10,30 @@
 #
 
 if(NOT TARGET qi::qi)
+  # Look for libqi (as installed from lastest souce)
   find_package(qi QUIET)
   if(NOT ${QI_PACKAGE_FOUND})
-    message(FATAL_ERROR "Could not find the qi library")
+    # If not found, search using pkgconfig (e.g installed with ros-melodic-naoqi-libqi ros-melodic-naoqi-libqicore)
+    include(FindPkgConfig)
+    pkg_search_module(LIBQI naoqi_libqi naoqi_libqicore)
+    if(NOT LIBQI_FOUND)
+      message(FATAL_ERROR "Could not find the libqi package using CMake package or pkg-config")
+    endif()
+    foreach(LIB ${LIBQI_LIBRARIES})
+      find_library(${LIB}_FULL_PATH NAME ${LIB} HINTS ${LIBQI_LIBRARY_DIRS})
+      list(APPEND LIBQI_FULL_LIBRARIES ${${LIB}_FULL_PATH})
+    endforeach()
+    message("-- Found libqi libraries: ${LIBQI_FULL_LIBRARIES}")
+    add_library(qi::qi INTERFACE IMPORTED)
+    set_target_properties(qi::qi PROPERTIES
+      INTERFACE_LINK_LIBRARIES "${LIBQI_FULL_LIBRARIES}"
+    )
+    if(NOT "${LIBQI_INCLUDE_DIRS}" STREQUAL "")
+      set_target_properties(qi::qi PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBQI_INCLUDE_DIRS}"
+      )
+      message("-- Found libqi include directories: ${LIBQI_INCLUDE_DIRS}")
+    endif()
   else()
     add_library(qi::qi INTERFACE IMPORTED)
     set_target_properties(qi::qi PROPERTIES
